e2584e3b82e94f2648b73a52b40865886876104c
Preyanshu <mishrapreyanshu@gmail.com>
2024-07-03 13:26:19 +0530

    instructor fix
    Signed-off-by: Preyanshu <mishrapreyanshu@gmail.com>

--- src/app/dashboard/instructors/page.js
+++ src/app/dashboard/instructors/page.js
@@ -20,7 +20,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { InstructorActions } from '@/redux/slices';
 import TableSkeleton from '@/components/core/Skeletion';
-import PriviledgeForbidden from '@/components/core/PriviledgeForbidden';
+// import PriviledgeForbidden from '@/components/core/PriviledgeForbidden';
 
 
 export default function Page({ searchParams }) {
@@ -108,8 +108,8 @@ export default function Page({ searchParams }) {
         width: 'var(--Content-width)',
       }}
     >
-      {/* <Stack spacing={4}> */}
-        {/* <Stack direction={{ xs: 'column', sm: 'row' }} spacing={3} sx={{ alignItems: 'flex-start' }}>
+      <Stack spacing={4}>
+        <Stack direction={{ xs: 'column', sm: 'row' }} spacing={3} sx={{ alignItems: 'flex-start' }}>
           <Box sx={{ flex: '1 1 auto' }}>
             <Typography variant="h4">Instructors</Typography>
           </Box>
@@ -120,12 +120,12 @@ export default function Page({ searchParams }) {
               Add
             </Button>
           </Box>
-        </Stack> */}
+        </Stack>
 
 
-<PriviledgeForbidden action={'ADD VENDOR'} ></PriviledgeForbidden>
+{/* <PriviledgeForbidden action={'ADD VENDOR'} ></PriviledgeForbidden> */}
 
-        {/* <Stack direction="row" spacing={2} sx={{ px: 3, py: 2 }}>
+        <Stack direction="row" spacing={2} sx={{ px: 3, py: 2 }}>
           <OutlinedInput
             placeholder="Search Instructors"
             startAdornment={
@@ -156,8 +156,8 @@ export default function Page({ searchParams }) {
             onPageChange={handlePageChange}
             onRowsPerPageChange={handleRowsPerPageChange}
           />
-        </Card> */}
-      {/* </Stack> */}
+        </Card>
+      </Stack>
     </Box>
   );
 }
