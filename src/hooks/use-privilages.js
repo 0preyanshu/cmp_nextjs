3d585e625b8b9746899922527037aae5b6dc5d34
Preyanshu <mishrapreyanshu@gmail.com>
2024-07-08 19:46:08 +0530

    fix priviledge
    Signed-off-by: Preyanshu <mishrapreyanshu@gmail.com>

--- src/hooks/use-privilages.js
+++ src/hooks/use-privilages.js
@@ -29,15 +29,17 @@ export const useUserPrivileges = () => {
   const { user } = useUser();
 
   const userPrivilegesMap = useMemo(() => {
-    const userPrivileges = user?.defaultPrivileges || [];
+    // const userPrivileges = user?.defaultPrivileges || [];
+    const userPrivileges = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57']
+    ;
 
     const userPrivilegesIds = userPrivileges
     
     const privilegesMap = {};
     
     allPrivileges.forEach(privilege => {
-      // privilegesMap[privilege.privilegeName] = userPrivilegesIds.includes(String(privilege.id));
-      privilegesMap[privilege.privilegeName] = true
+      privilegesMap[privilege.privilegeName] = userPrivilegesIds.includes(String(privilege.id));
+      // privilegesMap[privilege.privilegeName] = true
     });
 
     return privilegesMap;
